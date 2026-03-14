5ecfcb60eaeb5cd84dc0f6b74b3b8ce7ed2caf53
KARELIN Artem <karelinskit@gmail.com>
2026-03-14 15:30:28 +0100

    Fixed bugs in the dark theme; fonts now display correctly

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// 1. Эффект печатающегося текста
 const textElement = document.getElementById('typing-text');
 const profession = "Designer UI/UX et Développeur";
 let index = 0;
@@ -7,47 +7,27 @@ function typeEffect() {
     if (index < profession.length) {
         textElement.textContent += profession.charAt(index);
         index++;
-
-        const speed = Math.random() * (150 - 50) + 50; 
-        setTimeout(typeEffect, speed); 
+        setTimeout(typeEffect, 100); // Скорость печати
     }
 }
 
-
+// 2. Переключение темной темы
 const themeBtn = document.getElementById('theme-toggle');
 const body = document.body;
-const icon = themeBtn.querySelector('i');
-
-function applyTheme(theme) {
-    if (theme === 'dark') {
-        body.classList.add('dark-mode');
-        if (icon) icon.classList.replace('fa-moon', 'fa-sun');
-    } else {і
-        body.classList.remove('dark-mode');
-        if (icon) icon.classList.replace('fa-sun', 'fa-moon');
-    }
-}
-
-const savedTheme = localStorage.getItem('theme') || 'light';
-applyTheme(savedTheme);
 
 themeBtn.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
     
-    const isDark = body.classList.contains('dark-mode');
-    const currentTheme = isDark ? 'dark' : 'light';
-    
-    
-    localStorage.setItem('theme', currentTheme);
-    
-    
-    if (isDark) {
+    // Меняем иконку
+    const icon = themeBtn.querySelector('i');
+    if (body.classList.contains('dark-mode')) {
         icon.classList.replace('fa-moon', 'fa-sun');
     } else {
         icon.classList.replace('fa-sun', 'fa-moon');
     }
 });
 
+// Запуск функций
 window.onload = () => {
     typeEffect();
 };
