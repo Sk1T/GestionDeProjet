308232197ae05ed0c0877375441cb5ed3076c8df
KARELIN Artem <karelinskit@gmail.com>
2026-03-14 15:07:40 +0100

    Fixed bugs in the dark theme and added support for the browser to remember the selected theme

--- script.js
+++ script.js
@@ -7,19 +7,41 @@ function typeEffect() {
     if (index < profession.length) {
         textElement.textContent += profession.charAt(index);
         index++;
-        setTimeout(typeEffect, 100); 
+
+        const speed = Math.random() * (150 - 50) + 50; 
+        setTimeout(typeEffect, speed); 
     }
 }
 
+
 const themeBtn = document.getElementById('theme-toggle');
 const body = document.body;
+const icon = themeBtn.querySelector('i');
+
+function applyTheme(theme) {
+    if (theme === 'dark') {
+        body.classList.add('dark-mode');
+        if (icon) icon.classList.replace('fa-moon', 'fa-sun');
+    } else {і
+        body.classList.remove('dark-mode');
+        if (icon) icon.classList.replace('fa-sun', 'fa-moon');
+    }
+}
+
+const savedTheme = localStorage.getItem('theme') || 'light';
+applyTheme(savedTheme);
 
 themeBtn.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
     
-   
-    const icon = themeBtn.querySelector('i');
-    if (body.classList.contains('dark-mode')) {
+    const isDark = body.classList.contains('dark-mode');
+    const currentTheme = isDark ? 'dark' : 'light';
+    
+    
+    localStorage.setItem('theme', currentTheme);
+    
+    
+    if (isDark) {
         icon.classList.replace('fa-moon', 'fa-sun');
     } else {
         icon.classList.replace('fa-sun', 'fa-moon');
